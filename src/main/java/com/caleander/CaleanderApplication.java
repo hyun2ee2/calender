00dc9ace02de8f01d6d1e1c0d72bc50ebe92df79
hyun2ee2 <Hongkobi04@gmail.com>
2026-02-02 15:18:01 +0900

    feat: 첫번째 커밋

---
+++ src/main/java/com/caleander/CaleanderApplication.java
@@ -0,0 +1,13 @@
+package com.caleander;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaleanderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaleanderApplication.class, args);
+    }
+
+}
