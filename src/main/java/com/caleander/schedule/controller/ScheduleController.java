329a6f6d7ef38a6c4bf23407a76949145c1de0cf
hyun2ee2 <Hongkobi04@gmail.com>
2026-02-03 22:32:23 +0900

    feat: ScheduleController

---
+++ src/main/java/com/caleander/schedule/controller/ScheduleController.java
@@ -0,0 +1,58 @@
+package com.caleander.schedule.controller;
+
+
+import com.caleander.schedule.dto.*;
+import com.caleander.schedule.service.ScheduleService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController("/calender")
+@RequiredArgsConstructor
+public class ScheduleController {
+    private final ScheduleService scheduleService;
+
+    // POST
+    @PostMapping
+    public ResponseEntity<CreateScheduleResponse> crateSchedule(@RequestBody CreateScheduleRequest request) {
+        CreateScheduleResponse result = scheduleService.save(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(result);
+    }
+
+    // 단 건 GET
+    @GetMapping("/{author}")
+    public ResponseEntity<GetOneScheduleResponse> getOneScheduleResponse(@PathVariable String author) {
+        GetOneScheduleResponse result = scheduleService.getOne(author);
+        return ResponseEntity.status(HttpStatus.OK).body(result);
+    }
+
+    // 다 건 GET
+    @GetMapping
+    public ResponseEntity<List<GetOneScheduleResponse>> getAllAuthor() {
+        List<GetOneScheduleResponse> result = scheduleService.getAll();
+        return ResponseEntity.status(HttpStatus.OK).body(result);
+    }
+
+    // PUT
+    @PutMapping("/{id}")
+    public ResponseEntity<UpdateScheduleResponse> update(
+            @PathVariable Long id,
+            @RequestBody UpdateScheduleRequest request
+    ) {
+        UpdateScheduleResponse result = scheduleService.update(id, request);
+        return ResponseEntity.status(HttpStatus.OK).body(result);
+    }
+
+    // DELETE
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(
+            @PathVariable Long id,
+            @RequestBody DeleteScheduleRequest request
+    ) {
+        scheduleService.delete(id, request);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+}
