59145c63fa85830ed68e528ac05fc6857111ce5d
hyun2ee2 <Hongkobi04@gmail.com>
2026-02-04 12:24:11 +0900

    feat: CreateScheduleRequest

--- src/main/java/com/caleander/schedule/dto/CreateScheduleRequest.java
+++ src/main/java/com/caleander/schedule/dto/CreateScheduleRequest.java
@@ -1,8 +1,11 @@
 package com.caleander.schedule.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
+@AllArgsConstructor
 public class CreateScheduleRequest {
 
     private String title;
